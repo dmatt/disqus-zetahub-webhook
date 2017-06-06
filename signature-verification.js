fcd06b839c8afe27adf766a206b964471a154820
System <none>
2017-06-06 15:29:10 +0000

    Checkpoint

--- signature-verification.js
+++ signature-verification.js
@@ -1,7 +1,5 @@
 const crypto = require('crypto')
 
-// const secretKey = Buffer.from(, 'utf8')
-
 module.exports = {
   getHash: (data) => {
     return crypto.createHmac('sha256', process.env.WEBHOOKS_SECRET_KEY)
